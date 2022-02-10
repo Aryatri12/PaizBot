8a75141010ea9a73c41761525b2c9e444dde585e
Paizx <faizfedinand59@gmail.com>
2022-02-10 21:01:02 +0700

    Alphabot

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY Paiz
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 0812 4908 6533
+2. BCA 
+	ㅁ 0812 4908 6533
+3. BNI 
+	ㅁ 0812 4908 6533
+4. PERMATA
+	ㅁ 0812 4908 6533
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0812 4908 6533
+2. OVO
+	ㅁ 0812 4908 6533
+3. DANA
+	ㅁ 0812 4908 6533
+	
+Semuanya atas nama gataulupasaya
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 0812 4908 6533
+2. BCA 
+	ㅁ 0812 4908 6533
+3. BNI 
+	ㅁ 0812 4908 6533
+4. PERMATA
+	ㅁ 0812 4908 6533
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0812 4908 6533
+2. OVO
+	ㅁ 0812 4908 6533
+3. DANA
+	ㅁ 0812 4908 6533
+	
+Semuanya atas nama gatau
+`
+	}
+
+exports.donasibot = donasibot
+ 
